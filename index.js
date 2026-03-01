0a4abf95d52bdad4018aaffb6531281a93443a4c
Yousuf007 <yousufjobaer@gmail.com>
2026-03-02 03:25:28 +0600

    build

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const openai = new OpenAI({
 });
 
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", 'https://hospital-management-vert-nu.vercel.app'],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 
